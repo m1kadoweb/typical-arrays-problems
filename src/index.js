9afd6c2e2f92f05d66171ab48bcebd3e9404b9f3
m1kadoweb <37939676+m1kadoweb@users.noreply.github.com>
2020-09-30 10:00:56 +0500

    refactor: improve code

--- src/index.js
+++ src/index.js
@@ -1,52 +1,37 @@
 
 exports.min = function min (array) {
-  let minNum = 0;
-  if(array === undefined) {
-    return 0;
+  let minNum = 0
+  if(array === undefined || array.length === 0) {
+    return 0
+  } else {
+    array.forEach(num => {
+      minNum > num ? minNum = num : ''
+    });
   }
-  
-  for (let index = 0; index < array.length; index++) {
- 
-    if(array.length == 0) {
-      return 0;
-    }
-
-    else if(minNum > array[index]) {
-      minNum = array[index];
-    }
-    }
-  return minNum;
+  return minNum
 }
 
 exports.max = function max (array) {
-  let maxNum = 0;
-  if(array === undefined) {
-    return 0;
+  let maxNum = 0
+  if(array === undefined || array.length === 0) {
+    return 0
+  } else {
+    array.forEach(num => {
+      maxNum < num ? maxNum = num : ''
+    });
   }
-  for (let index = 0; index < array.length; index++) {
-    
-    if(array.length == 0) {
-      return 0;
-    }
-
-    else if(maxNum < array[index]) {
-      maxNum = array[index];
-    }
-  }
-  return maxNum;
+  return maxNum
 }
 
-exports.avg = function avg (array) {
-  let avgNum = 0;
-  if(array === undefined) {
-    return 0;
-  }
-  if(array.length == 0) {
-    return 0;
-  }
 
-  for (let index = 0; index < array.length; index++) {
-    avgNum = avgNum + array[index];   
-  }
-  return avgNum / array.length;
+exports.avg = function avg (array) {
+  let avgNums = 0
+  if(array === undefined || array.length === 0) {
+    return 0
+  } 
+
+  array.forEach(num => {
+    avgNums += num
+  })
+  return avgNums / array.length
 }
